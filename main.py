1ae6d74946aec2f0394427cfbb55384896947eda
primaeval <primaevaldev@gmail.com>
2019-03-09 18:11:10 +0100

    stat

--- main.py
+++ main.py
@@ -3046,8 +3046,12 @@ def get_free_space_mb(dirname):
         ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(dirname), None, None, ctypes.pointer(free_bytes))
         return free_bytes.value / 1024 / 1024
     else:
-        st = os.statvfs(dirname)
-        return st.f_bavail * st.f_frsize / 1024 / 1024
+        try:
+            st = os.statvfs(dirname)
+            return st.f_bavail * st.f_frsize / 1024 / 1024
+        except:
+            log(dirname)
+            return
 
 
 @plugin.route('/')
